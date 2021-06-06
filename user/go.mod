f0e14d6f2b1767f14f3e01f3c535cac881eee378
wangweicheng <wangweicheng@bilibili.com>
2021-06-06 16:42:49 +0800

    增加配置中心和注册中心

--- user/go.mod
+++ user/go.mod
@@ -3,12 +3,24 @@ module user
 go 1.15
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/golang/protobuf v1.4.3
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/gorm v1.9.16
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/micro/go-micro/v2 v2.9.1
-	github.com/micro/micro/v3 v3.0.0
+	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1
+	github.com/micro/go-plugins/registry/consul/v2 v2.9.1
+	github.com/stretchr/testify v1.6.1 // indirect
+	go.etcd.io/bbolt v1.3.5 // indirect
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3 // indirect
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
 
 // This can be removed once etcd becomes go gettable, version 3.4 and 3.5 is not,
--- user/main.go
+++ user/main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-plugins/registry/consul/v2"
+	"user/common"
 	"user/domain/repository"
 	service2 "user/domain/service"
 	"user/handler"
@@ -12,12 +16,28 @@ import (
 )
 
 func main() {
+	// 配置中心
+	consulConfig, err := common.GetConsulConfig("127.0.0.1", 8500, "/micro/config")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	// 注册中心
+	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.1:8500",
+		}
+	})
+
 	src := micro.NewService(
 		micro.Name("go.micro.service.user"),
 		micro.Version("lastest"),
+		micro.Address("127.0.0.1:8082"),
+		micro.Registry(consulRegistry),
 	)
 	src.Init()
-	db, err := gorm.Open("mysql", "root:123456@/micro?charset=utf8&parseTime=True&loc=Local")
+	mysqlInfo := common.GetMySqlFromConsul(consulConfig, "mysql")
+	db, err := gorm.Open("mysql", mysqlInfo.User+":"+mysqlInfo.Pwd+"@/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 		return
